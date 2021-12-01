ec981ef45212ce2d5180c8322395c74fda312812
Florentina Tache <florentina.tache8@gmail.com>
2021-12-01 16:59:34 +0200

    Basic project setup

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connection to the DataBase
+connectDB();
+
+app.get("/", (req, res) => res.send("API Running"));
+
+//Middleware
+
+app.use(express.json({ extended: false }));
+
+// Define the Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
